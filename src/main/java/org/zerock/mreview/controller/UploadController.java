b820bf9906cf30e49abac87c54ebefa37b798184
Kim_MS <rlaaudtlr145@gmail.com>
2022-06-29 19:55:00 +0900

    파일 확장자 Check

--- src/main/java/org/zerock/mreview/controller/UploadController.java
+++ src/main/java/org/zerock/mreview/controller/UploadController.java
@@ -6,6 +6,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
+
 @RestController
 @Log4j2
 public class UploadController {
@@ -17,13 +25,56 @@ public class UploadController {
     public void uploadFile(MultipartFile[] uploadFiles) {
 
         for (MultipartFile uploadFile : uploadFiles) {
+
+            // 이미지 파일이 아니면 오류 발생시킴
+            if (uploadFile.getContentType().startsWith("image") == false) {
+                log.warn("this file is not image type");
+                return;
+            }
+
             // 실제 파일 이름 IE나 Edge는 전체 경로가 들어오므로
             String originalName = uploadFile.getOriginalFilename();
             String fileName = originalName.substring(originalName.lastIndexOf("\\") + 1);
 
             log.info("fileName : " + fileName);
+
+            // 날짜 폴더 생성
+            String folderPath = makeFolder();
+
+            // UUID
+            String uuid = UUID.randomUUID().toString();
+
+            // 저장할 파일 이름 중간에 "_"를 이용해서 구분
+            String saveName = uploadPath + File.separator + folderPath + File.separator + uuid + "_" + fileName;
+
+            Path savePath = Paths.get(saveName);
+
+            try {
+
+                uploadFile.transferTo(savePath);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         } // end for
 
+    }
+
+    private String makeFolder() {
+
+        String str = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy/mm/dd"));
+
+        String folderPath = str.replace("/", File.separator);
+
+        // make Folder --------
+        File uploadPathFolder = new File(uploadPath, folderPath);
+
+        if (uploadPathFolder.exists() == false) {
+            uploadPathFolder.mkdirs();
+        }
+
+        return folderPath;
     }
 
 }
